d49277618eef9446ee9cc31ecc20bdd3ad473dca
Ishan Dwivedi <dwivediishan75@gmail.com>
2023-01-17 18:31:46 +0530

    Feat: Workspace

--- tests/workspaceTest.spec.ts
+++ tests/workspaceTest.spec.ts
@@ -1,8 +1,21 @@
-import { test, expect } from '@playwright/test';
-import Mailosaur from 'mailosaur'
+const playwright = require('playwright');
+const assert = require('chai').assert;
+const Mailosaur = require('mailosaur');
 
+(async () => {
+  const apiKey = 'EtxwAfP5G3GN2RE2';
+  const mailosaur = new Mailosaur(apiKey);
 
-test('test', async ({ page }) => {
+  const serverId = '0njdz63p';
+  const serverDomain = 'fresh-five@0njdz63p.mailosaur.net';
+
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+    
+
+  await page.goto('https://auth.oslash.com/passwordless/wl8wTn896ogMzUxYqv1HE5jqs/confirm');
+  await page.goto('https://app.oslash.com/auth/callback/passwordless?loginMethod=email&platform=Chrome&code=01GPZVBRC017C3BH3NY0900DSH');
   await page.goto('https://app.oslash.com/signup/onboarding');
   await page.getByPlaceholder('Your workspace name').click();
   await page.getByPlaceholder('Your workspace name').fill('Chain-Saw');
@@ -15,4 +28,6 @@ test('test', async ({ page }) => {
   await page.getByLabel('Set Workspace Icon').setInputFiles('jedi.jpeg');
   await page.locator('button[name="save"]').click();
   await page.getByRole('button', { name: 'Save' }).click();
-});
+  await browser.close();
+})
+();
