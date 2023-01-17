4d114846fa2651a988947cec75306354f284c15d
Ishan Dwivedi <dwivediishan75@gmail.com>
2023-01-17 18:46:51 +0530

    Workspace

--- tests/workspaceTest.spec.ts
+++ tests/workspaceTest.spec.ts
@@ -1,18 +1,12 @@
-const playwright = require('playwright');
-const assert = require('chai').assert;
-const Mailosaur = require('mailosaur');
+import { test, expect } from '@playwright/test';
+import Mailosaur from 'mailosaur';
 
-(async () => {
+test('test', async ({ page }) =>  {
   const apiKey = 'EtxwAfP5G3GN2RE2';
   const mailosaur = new Mailosaur(apiKey);
 
   const serverId = '0njdz63p';
   const serverDomain = 'fresh-five@0njdz63p.mailosaur.net';
-
-  const browser = await playwright.chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-    
 
   await page.goto('https://auth.oslash.com/passwordless/wl8wTn896ogMzUxYqv1HE5jqs/confirm');
   await page.goto('https://app.oslash.com/auth/callback/passwordless?loginMethod=email&platform=Chrome&code=01GPZVBRC017C3BH3NY0900DSH');
@@ -28,6 +22,4 @@ const Mailosaur = require('mailosaur');
   await page.getByLabel('Set Workspace Icon').setInputFiles('jedi.jpeg');
   await page.locator('button[name="save"]').click();
   await page.getByRole('button', { name: 'Save' }).click();
-  await browser.close();
-})
-();
+});
