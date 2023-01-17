d82f4ec6eec13d830c1635b5d47efeb18c9e03d9
Ishan Dwivedi <dwivediishan75@gmail.com>
2023-01-17 13:06:25 +0530

    Magik Link Login

---
+++ tests/MagicLinkTest.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://auth.oslash.com/passwordless/G09wdjoYqEiFOwRdsnPfWDlMS/confirm');
+  await page.goto('https://app.oslash.com/auth/callback/passwordless?loginMethod=email&platform=Chrome&code=01GPZA4NWXRJR9J11FXT16N35M');
+  await page.goto('https://app.oslash.com/login/install?platform=Chrome');
+  await page.goto('https://app.oslash.com/org?platform=Chrome');
+  await page.goto('https://app.oslash.com/shortcuts/all');
+});
+
+test('first', async ({ page }) => {
+    // page is signed in.
+  });
+  
+  test('second', async ({ page }) => {
+    // page is signed in.
+  });
