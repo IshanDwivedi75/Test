ea0a3f58fef2d203ee449928c3cbbd08627efc73
Ishan Dwivedi <dwivediishan75@gmail.com>
2023-01-17 16:38:03 +0530

    Feat: Workspace Setup

--- tests/workspaceTest.spec.ts
+++ tests/workspaceTest.spec.ts
@@ -1,16 +1,18 @@
 import { test, expect } from '@playwright/test';
- 
+
 test('test', async ({ page }) => {
- await page.goto('https://auth.oslash.com/passwordless/HEeiKXHXn0WDJBi8wA4gmL0Xr/confirm');
- await page.goto('https://app.oslash.com/auth/callback/passwordless?loginMethod=email&platform=Chrome&code=01GPZJ3F78HAP2WMH81Q6JNT5X');
- await page.goto('https://app.oslash.com/login/install?platform=Chrome');
- await page.goto('https://app.oslash.com/org?platform=Chrome');
- await page.goto('https://app.oslash.com/shortcuts/all');
- await page.getByRole('menuitem', { name: 'View profile' }).click();
- await page.getByPlaceholder('Your display name').click();
- await page.getByText('Upload your photo').click();
- await page.getByLabel('Upload your photo').setInputFiles('jedi.jpeg');
- await page.locator('.cropper-face').click();
- await page.locator('button[name="save"]').click();
- await page.getByRole('button', { name: 'Save' }).click();
+  
+  await page.getByRole('button', { name: 'workspacesidenavbaritem' }).click();
+  await page.getByPlaceholder('Search').click();
+  await page.getByPlaceholder('Search').fill('eh');
+  await page.getByPlaceholder('Search').press('Enter');
+  await page.getByRole('button', { name: 'clear' }).click();
+  await page.getByRole('link', { name: 'Settings & Members' }).click();
+  await page.getByPlaceholder('Your team or company name').click();
+  await page.getByPlaceholder('Your team or company name').click();
+  await page.getByText('Set Workspace Icon').click();
+  await page.getByLabel('Set Workspace Icon').setInputFiles('jedi.jpeg');
+  await page.locator('button[name="save"]').click();
+  await page.getByRole('button', { name: 'Save' }).click();
+  await page.getByText('Workspace settings updated successfully').click();
 });
