558164ed657edd249cfd19efed91e07d5bd63e40
Ishan Dwivedi <dwivediishan75@gmail.com>
2023-01-17 15:18:01 +0530

    Login Test

---
+++ tests/MagikLogin.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://app.oslash.com/login');
+  await page.getByPlaceholder('name@email.com').click();
+  await page.getByPlaceholder('name@email.com').fill('dwivediishan75@gmail.com');
+  await page.getByRole('button', { name: 'Send' }).click();
+  await page.locator('.space-y-6').click();
+});
