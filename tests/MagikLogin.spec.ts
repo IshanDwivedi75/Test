1b75dd12d75ed87e4c7c8dca9e2d5b036603e25e
Ishan Dwivedi <110477856+IshanDwivedi75@users.noreply.github.com>
2023-01-17 15:37:00 +0530

    Delete MagikLogin.spec.ts

--- tests/MagikLogin.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://app.oslash.com/login');
-  await page.getByPlaceholder('name@email.com').click();
-  await page.getByPlaceholder('name@email.com').fill('dwivediishan75@gmail.com');
-  await page.getByRole('button', { name: 'Send' }).click();
-});
